b8200b4ecf417223cc3031ab5e11ebe309de61cd
Пользователь <otabeksediyorov298@gmail.com>
2024-06-28 08:50:12 +0500

    tayyor

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".navbar__collection");
+
+function toggleShow() {
+  navbar.classList.toggle("show");
+}
+const navbar__btr = document.querySelector("button");
+function togglepow() {
+  navbar__btr.classList.toggle("pow");
+}
